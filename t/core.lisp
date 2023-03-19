a724ced6a8421c314a355fca1bae10cc934634d1
Alexander Artemenko <svetlyak.40wt@gmail.com>
2023-03-19 07:40:03 +0000

    Removed force because it can't be used in nested calls to asdf:operate in ASDF >= 3.3.6.

--- t/core.lisp
+++ t/core.lisp
@@ -43,7 +43,7 @@
                             tmp-path)))
       (let* ((asdf:*central-registry* (cons tmp-path
                                             asdf:*central-registry*))
-             (resulting-path (progn (asdf:load-system docs-system-name :force t)
+             (resulting-path (progn (asdf:load-system docs-system-name)
                                     (docs-builder:build docs-system-name)))
              (index.html (merge-pathnames
                           (make-pathname :name "index"
@@ -84,6 +84,6 @@
       (let* ((asdf:*central-registry* (cons tmp-path
                                             asdf:*central-registry*))
              (rove:*default-reporter* :none))
-        (asdf:load-system tests-system-name :force t)
+        (asdf:load-system tests-system-name)
         
         (asdf:test-system lib-name)))))
